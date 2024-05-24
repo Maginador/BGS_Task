730251c5ec53af842c5bb51338796e7b785441f4
Maginador-Nitro\Ricardo Maginador <maginador@gmail.com>
2024-05-24 07:31:12 -0300

    Adds store preview, creates store products

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -7,10 +7,14 @@ public class StoreManager : MonoBehaviour
     [SerializeField] ProductSO[] storeProducts;
     [SerializeField] GridButton gridButtonPrefab;
     [SerializeField] Transform storeGridRoot;
+    [SerializeField] StorePreviewer previewer;
+    private void Start(){
+        BuildUI();
+    }
     private void BuildUI(){
         for(int i =0; i < storeProducts.Length; i++){
             var button = Instantiate(gridButtonPrefab,storeGridRoot);
-            button.Setupbutton(storeProducts[i]);
+            button.Setupbutton(storeProducts[i], previewer.ShowPreview);
         }
     }
 }
---
+++ Assets/StorePreviewer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StorePreviewer : MonoBehaviour
+{
+
+    [SerializeField] Image hatSlot, chestSlot, rGloveSlot, lGloveSlot;
+
+
+
+    public void ClearPreview()
+    {
+
+        hatSlot.sprite = null;
+        hatSlot.enabled = false;
+
+        chestSlot.sprite = null;
+        chestSlot.enabled = false;
+
+        rGloveSlot.sprite = null;
+        rGloveSlot.enabled = false;
+
+        lGloveSlot.sprite = null;
+        lGloveSlot.enabled = false;
+    }
+    public void ShowPreview(ProductSO so)
+    {
+        ClearPreview();
+        switch (so.category)
+        {
+            case ProductSO.Category.Hat:
+                hatSlot.sprite = so.asset;
+                hatSlot.enabled = true;
+                break;
+            case ProductSO.Category.Chest:
+                chestSlot.sprite = so.asset;
+                chestSlot.enabled = true;
+                break;
+
+            case ProductSO.Category.Glove:
+                rGloveSlot.sprite = so.asset;
+                rGloveSlot.enabled = true;
+                lGloveSlot.sprite = so.asset;
+                lGloveSlot.enabled = true;
+                break;
+        }
+
+    }
+}
